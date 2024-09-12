f47f7fab3cba520392d5495c4ea736756db667c0
kumar harsh <hk995814@gmail.com>
2024-09-13 01:09:45 +0530

    phone

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       //  home: firsthome(),
-      initialRoute: '/phone_auth2',
+      initialRoute: '/gmail_auth',
       routes: {
         '/': (context) => firsthome(),
         '/secondone': (context) => secondone(),
