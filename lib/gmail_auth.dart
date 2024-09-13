1c51d196627f7d1c3cb71f683677acafeaa2288f
kumar harsh <hk995814@gmail.com>
2024-09-14 00:47:51 +0530

    gmail auth

--- lib/gmail_auth.dart
+++ lib/gmail_auth.dart
@@ -15,99 +15,221 @@ class _gmail_authState extends State<gmail_auth> {
     double screenwidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-        body: Center(
-          child: Container(
-            // color: Colors.red,
-          width: screenwidth*0.9,
-          height: screenheight*1.0,
-          child: Column(children: [
-            Container(
-              padding: EdgeInsets.only(top:20),
-              width: screenwidth*0.85,
-              child: Icon(Icons.connect_without_contact_sharp,
-                size: 35,
-                color: Colors.black,),
-            ),
-            SizedBox(height: 20),
-            Container(
-             
-              width: screenwidth*0.85,
-              child: Center(child: Text('Log in to Chatbox',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),)),
-            ),
-                        SizedBox(height: 10,),
-Container(
-             
-              width: screenwidth*0.85,
-              child: Center(child: Text('Welcome back! Sign in using your social ',style: TextStyle(color: Color(0xFF797C7B)),)),
-            ),
-
-            Container(
-             
-              width: screenwidth*0.85,
-              child: Center(child: Text('account or email to continue us',style: TextStyle(color: Color(0xFF797C7B)),)),
-            ),
-                        SizedBox(height: 25),
-                  Container(
-                    width: screenwidth*0.85,
-                    child:Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                       children: [
-                         FloatingActionButton(
-                          child: FaIcon(
-                            FontAwesomeIcons.facebook,
-                            color: Colors.black,
-                          ),
-                          backgroundColor: Colors.white,
-                          hoverColor: const Color.fromARGB(255, 230, 174, 92),
-                          focusColor: Color.fromARGB(255, 92, 204, 96),
-                          splashColor: const Color.fromARGB(255, 203, 109, 219),
-                          elevation: 10,
-                          foregroundColor: Color.fromARGB(255, 218, 54, 54),
-                          onPressed: () =>
-                              {},
-                          shape: RoundedRectangleBorder(
-                              side: BorderSide(width: 3, color: Colors.black),
-                              borderRadius: BorderRadius.circular(100))),
-                              SizedBox(
-                        width: 15,
-                      ),
-                      FloatingActionButton(
-                          child: FaIcon(
-                            FontAwesomeIcons.google,
-                            color: Colors.black,
-                          ),
-                          backgroundColor: Colors.white,
-                          hoverColor: const Color.fromARGB(255, 230, 174, 92),
-                          focusColor: Color.fromARGB(255, 92, 204, 96),
-                          splashColor: const Color.fromARGB(255, 203, 109, 219),
-                          elevation: 10,
-                          onPressed: () => {},
-                          shape: RoundedRectangleBorder(
-                              side: BorderSide(width: 3, color: Colors.black),
-                              borderRadius: BorderRadius.circular(100))),
-                              SizedBox(
-                        width: 15,
-                      ),
-                      FloatingActionButton(
-                          child: FaIcon(
-                            FontAwesomeIcons.apple,
-                            color: Colors.black,
-                          ),
-                          backgroundColor: Colors.white,
-                          hoverColor: const Color.fromARGB(255, 230, 174, 92),
-                          focusColor: Color.fromARGB(255, 92, 204, 96),
-                          splashColor: const Color.fromARGB(255, 203, 109, 219),
-                          elevation: 10,
-                          onPressed: () => {},
-                          shape: RoundedRectangleBorder(
-                              side: BorderSide(width: 3, color: Colors.black),
-                              borderRadius: BorderRadius.circular(100)))
-                       ],
+      body: Center(
+        child: Container(
+          // color: Colors.red,
+          width: screenwidth * 0.9,
+          height: screenheight * 1.0,
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.only(top: 20),
+                width: screenwidth * 0.85,
+                child: Icon(
+                  Icons.connect_without_contact_sharp,
+                  size: 35,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                width: screenwidth * 0.85,
+                child: Center(
+                    child: Text(
+                  'Log in to Chatbox',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                )),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: screenwidth * 0.85,
+                child: Center(
+                    child: Text(
+                  'Welcome back! Sign in using your social ',
+                  style: TextStyle(color: Color(0xFF797C7B)),
+                )),
+              ),
+              Container(
+                width: screenwidth * 0.85,
+                child: Center(
+                    child: Text(
+                  'account or email to continue us',
+                  style: TextStyle(color: Color(0xFF797C7B)),
+                )),
+              ),
+              SizedBox(height: 25),
+              Container(
+                width: screenwidth * 0.85,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FloatingActionButton(
+                        child: FaIcon(
+                          FontAwesomeIcons.facebook,
+                          color: Colors.black,
+                        ),
+                        backgroundColor: Colors.white,
+                        hoverColor: const Color.fromARGB(255, 230, 174, 92),
+                        focusColor: Color.fromARGB(255, 92, 204, 96),
+                        splashColor: const Color.fromARGB(255, 203, 109, 219),
+                        elevation: 10,
+                        foregroundColor: Color.fromARGB(255, 218, 54, 54),
+                        onPressed: () => {},
+                        shape: RoundedRectangleBorder(
+                            side: BorderSide(width: 3, color: Colors.black),
+                            borderRadius: BorderRadius.circular(100))),
+                    SizedBox(
+                      width: 15,
                     ),
-                  )
-          ],),
+                    FloatingActionButton(
+                        child: FaIcon(
+                          FontAwesomeIcons.google,
+                          color: Colors.black,
+                        ),
+                        backgroundColor: Colors.white,
+                        hoverColor: const Color.fromARGB(255, 230, 174, 92),
+                        focusColor: Color.fromARGB(255, 92, 204, 96),
+                        splashColor: const Color.fromARGB(255, 203, 109, 219),
+                        elevation: 10,
+                        onPressed: () => {},
+                        shape: RoundedRectangleBorder(
+                            side: BorderSide(width: 3, color: Colors.black),
+                            borderRadius: BorderRadius.circular(100))),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    FloatingActionButton(
+                        child: FaIcon(
+                          FontAwesomeIcons.apple,
+                          color: Colors.black,
+                        ),
+                        backgroundColor: Colors.white,
+                        hoverColor: const Color.fromARGB(255, 230, 174, 92),
+                        focusColor: Color.fromARGB(255, 92, 204, 96),
+                        splashColor: const Color.fromARGB(255, 203, 109, 219),
+                        elevation: 10,
+                        onPressed: () => {},
+                        shape: RoundedRectangleBorder(
+                            side: BorderSide(width: 3, color: Colors.black),
+                            borderRadius: BorderRadius.circular(100)))
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                child: IntrinsicHeight(
+                    child: new Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: screenwidth * 0.38,
+                      height: 2,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      "OR",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Container(
+                      width: screenwidth * 0.38,
+                      height: 2,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ],
+                )),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Container(
+                // color: Colors.red,
+                width: screenwidth * 0.85,
+                height: 20,
+                child: Text(
+                  'Your email',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF797C7B),
+                  ),
+                ),
+              ),
+              Container(
+                width: screenwidth * 0.85,
+                child: TextField(
+                  decoration: InputDecoration(
+                      // hintText: 'Enter a search term',    // border: OutlineInputBorder(),
+
+                      ),
+                ),
+              ),
+              SizedBox(height: 20,),
+              Container(
+                // color: Colors.red,
+                width: screenwidth * 0.85,
+                height: 20,
+                    child: Text(
+                      'Password',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF797C7B),
+                      ),
+                    ),
+              ),
+              Container(
+                width: screenwidth * 0.85,
+                child: TextField(
+                  decoration: InputDecoration(
+                      // hintText: 'Enter a search term',    // border: OutlineInputBorder(),
+
+                      ),
+                ),
+              ),
+              SizedBox(height: 50,),
+                Container(
+                  width: screenwidth*0.70,
+                  height:40,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      
+                    },
+                    child: Text('verify',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize:20),),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor:Color(0xFF24786D),
+                    ),
+                  ),
+                
+                ),
+                SizedBox(height: 15,),
+                
+                   Container(
+                    width: screenwidth*0.30,
+                    child: TextButton(onPressed: () {
+                      
+                    },
+                    child: Text('Forget password?',style: TextStyle(color: Color(0xFF24786D),fontWeight: FontWeight.bold),),),
+                    )
+                
+            ],
           ),
         ),
+      ),
     );
   }
 }
